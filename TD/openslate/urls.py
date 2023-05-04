cd3feb04243a9f632bbc19eee3ab02f555a2b556
Mahesh Birajdar <98763822+MaheshBBirajdar@users.noreply.github.com>
2023-05-04 18:07:43 +0530

    Update urls.py

--- TD/openslate/urls.py
+++ TD/openslate/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
     path('', include('blog.urls')),
+    path('', include('sample.urls')),
 
 #    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
 #    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
