81f35704012a5a158f9c5b1e30501c4a33764a5c
Mahesh Birajdar <98763822+MaheshBBirajdar@users.noreply.github.com>
2023-06-09 10:57:51 +0530

    Delete TD directory

--- TD/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Artist)
-admin.site.register(Shot)
-admin.site.register(IssuedShot)
-
-admin.site.register(Artist1)
-admin.site.register(Shot1)
